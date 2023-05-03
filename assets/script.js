67784e8a3f2adde58c47aac19097f62ba36b24aa
SadeJoseph <feojoseph1@gmail.com>
2023-05-03 15:42:29 +0100

    clear and delete function

--- assets/script.js
+++ assets/script.js
@@ -15,3 +15,14 @@ let Answer =()=> {
 
        alert("Enter a valid calculation")
     }
+    //clear date on screen when AC clicked
+function clr() {
+    answer.value= "";
+}
+// delete single character from input box 
+function del() {
+    //slice end value
+    answer.value= answer.value.slice(0, -1);
+
+}
+}
