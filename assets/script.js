898de244d29f28caa47f2ca002a9442adb995f16
SadeJoseph <feojoseph1@gmail.com>
2023-05-03 14:56:24 +0100

    solved styling error started js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,17 @@
+//grab input box element. 
+let answer = document.getElementById("screen");
+//to add number clicked into input box
+let calculate=(num)=>{
+    answer.value+=num;
+}
+// perform operation
 
+let Answer =()=> {
+    //evaluate and stop invalid operation ie * then - isnt valid. 
+    try{
+     answer.value=eval(answer.value)
+    }
+    catch(err){
 
-
-
+       alert("Enter a valid calculation")
+    }
